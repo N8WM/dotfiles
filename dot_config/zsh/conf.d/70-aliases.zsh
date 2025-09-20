0f8ba6e3d7de80f1c4aa430940261c40c53631a9
Nathan McCutchen <n8wm@icloud.com>
2025-09-19 21:58:42 -0700

    update

---
+++ dot_config/zsh/conf.d/70-aliases.zsh
@@ -0,0 +1,9 @@
+alias omp="oh-my-posh"
+
+alias vnew="mkdir .venv && python3 -m venv ./.venv"
+alias vact="source ./.venv/bin/activate"
+alias vdeact="deactivate"
+alias vrm="rm -rf .venv"
+alias vclean="pip freeze | xargs pip uninstall -y"
+
+alias inv='vim $(fzf -m --preview="bat --color=always {}")'
