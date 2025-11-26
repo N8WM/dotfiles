6bba705245a4287bde2c81ad0ad7e97afde37131
Nathan McCutchen <n8wm@icloud.com>
2025-11-26 00:17:49 -0800

    update

--- dot_config/nvim/lua/plugins/lspconfig.lua
+++ dot_config/nvim/lua/plugins/lspconfig.lua
@@ -1,17 +1,50 @@
 return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    inlay_hints = { enabled = false },
-    servers = {
-      eslint = {
-        settings = {
-          rulesCustomizations = {
-            {
-              rule = "*",
-              severity = "off",
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = { enabled = false },
+      servers = {
+        eslint = {
+          settings = {
+            rulesCustomizations = {
+              {
+                rule = "*",
+                severity = "off",
+              },
             },
           },
         },
+      },
+    },
+  },
+  {
+    "mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = {
+        "arduino-language-server",
+        "bash-language-server",
+        "beautysh",
+        "black",
+        "css-lsp",
+        "docker-compose-language-service",
+        "docker-language-server",
+        "dockerfile-language-server",
+        "eslint-lsp",
+        "eslint_d",
+        "hadolint",
+        "json-lsp",
+        "lua-language-server",
+        "markdown-toc",
+        "markdownlint-cli2",
+        "marksman",
+        "postgres-language-server",
+        "prettierd",
+        "prisma-language-server",
+        "shfmt",
+        "stylua",
+        "tree-sitter-cli",
+        "typescript-language-server",
+        "vtsls",
       },
     },
   },
