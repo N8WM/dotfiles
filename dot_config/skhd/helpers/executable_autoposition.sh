3760c5eee6a626723a9499c06c60fc3aa4a022a1
Nathan McCutchen <n8wm@icloud.com>
2026-02-20 21:56:28 -0800

    reposition filter

--- dot_config/skhd/helpers/executable_autoposition.sh
+++ dot_config/skhd/helpers/executable_autoposition.sh
@@ -1,16 +1,26 @@
 RANGE=80
 WINDOW="${YABAI_WINDOW_ID:-}"
 
-IS_FLOATING=$(yabai -m query --windows --window | jq -r '."is-floating"')
-SPACE_TYPE=$(yabai -m query --spaces --space | jq -r '.type')
+WINDOWQ="$(yabai -m query --windows --window $WINDOW)"
 
-[[ $IS_FLOATING == "false" ]] && [[ $SPACE_TYPE != "float" ]] && exit 0
+IS_FLOATING="$(jq -r '."is-floating"' <<<"$WINDOWQ")"
+IS_VISIBLE="$(jq -r '."is-visible"' <<<"$WINDOWQ")"
+CAN_RESIZE="$(jq -r '."can-resize"' <<<"$WINDOWQ")"
+CAN_MOVE="$(jq -r '."can-move"' <<<"$WINDOWQ")"
+
+SPACE="$(jq -r '."space"' <<<"$WINDOWQ")"
+SPACE_TYPE="$(yabai -m query --spaces --space $SPACE | jq -r '.type')"
+
+[[ "$IS_FLOATING" == "false" ]] && [[ "$SPACE_TYPE" != "float" ]] && exit 0
+[[ "$IS_VISIBLE" == "false" ]] && exit 0
+[[ "$CAN_RESIZE" == "false" ]] && exit 0
+[[ "$CAN_MOVE" == "false" ]] && exit 0
 
 yabai -m window $WINDOW --grid 6:6:1:1:4:4
 
-WINDOW_COUNT=$(yabai -m query --windows --space | jq 'length')
+WINDOW_COUNT="$(yabai -m query --windows --space $SPACE | jq 'length')"
 
-[[ $WINDOW_COUNT -lt 2 ]] && exit 0
+[[ "$WINDOW_COUNT" -lt 2 ]] && exit 0
 
 X_OFFSET=$((RANDOM % (RANGE * 2 + 1) - RANGE))
 Y_OFFSET=$((RANDOM % (RANGE * 2 + 1) - RANGE))
--- dot_config/yabai/conf/private_app_spaces.sh
+++ dot_config/yabai/conf/private_app_spaces.sh
@@ -5,5 +5,3 @@ yabai -m rule --add label=app-space-discord app="^Discord$" space=^4
 yabai -m rule --add label=app-space-ghostty app="^Ghostty$" space=^2
 yabai -m rule --add label=app-space-messages app="^Messages$" space=^1
 yabai -m rule --add label=app-space-music app="^Music$" space=^1
-yabai -m rule --add label=app-space-vmware-fusion app="^VMware Fusion$" space=^7
-yabai -m rule --add label=app-space-crossover app="^CrossOver$" space=^5
