8cacd16060c87cc06e48c26ad58efac5637dd84a
Nathan McCutchen <n8wm@icloud.com>
2026-06-09 09:14:52 -0700

    update

--- dot_config/yabai/conf/private_managed.sh
+++ dot_config/yabai/conf/private_managed.sh
@@ -7,3 +7,4 @@ yabai -m rule --add label=app-manage-music app="^Music$" manage=off
 yabai -m rule --add label=app-manage-overwatch-exe app="^Overwatch.exe$" manage=off
 yabai -m rule --add label=app-manage-microsoft-teams app="^Microsoft Teams$" manage=off
 yabai -m rule --add label=app-manage-nordvpn app="^NordVPN$" manage=off
+yabai -m rule --add label=app-manage-netbird-ui app="^netbird-ui$" manage=off
